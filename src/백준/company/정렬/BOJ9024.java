04de9986dd23df3a06a92cd3b201778d377659bc
akdgns12 <akdgns12@naver.com>
2022-02-11 15:56:01 +0900

    이분탐색

---
+++ src/백준/company/정렬/BOJ9024.java
@@ -0,0 +1,67 @@
+package 백준.company.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ9024 {
+    // 두 수의 합 / 골드 5 / 이분탐색, 투포인터
+    static int n,k,min;
+    static int[] num;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t=Integer.parseInt(br.readLine());
+
+        while(t-->0) {
+            st=new StringTokenizer(br.readLine());
+
+            n=Integer.parseInt(st.nextToken());
+            k=Integer.parseInt(st.nextToken());
+            min=Integer.MAX_VALUE;
+            num=new int[n];
+
+            st=new StringTokenizer(br.readLine());
+            for(int i=0;i<n;i++)
+                num[i]=Integer.parseInt(st.nextToken());
+
+            Arrays.sort(num);
+
+            System.out.println(Two_Pointer());
+        }
+    }
+
+    static int Two_Pointer() {
+        int left=0;
+        int right=n-1;
+
+        int count=0;
+        while(left<right) {
+            int sum=num[left]+num[right]; // 두 수의 합
+
+            if(sum>=k) // 합이 k보다 크다면 큰 수 빼기 -> right--
+                right--;
+
+            else // 합이 k보다 작으면 더 큰 수 더하기 -> left++
+                left++;
+
+            /*
+             * min은 k와 k와 가까운 수의 차가 저장된 변수
+             * min이 갱신될 때 마다 k와 가까운 수의 개수를 구하는 count 초기화
+             */
+            if(Math.abs(k-sum)<min) {
+                min=Math.abs(k-sum);
+                count=1;
+            }
+            // k와 가까운 정수를 발견하면 count 증가
+            else if(Math.abs(k-sum)==min)
+                count++;
+        }
+
+        return count;
+    }
+}
+
