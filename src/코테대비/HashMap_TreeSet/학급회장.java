832dd44b9a37d3ce07ab12fda51fd84066778179
akdgns12 <akdgns12@naver.com>
2022-02-14 23:49:43 +0900

    HashMap_TreeSet

--- src/코테대비/HashMap_TreeSet/학급회장.java
+++ src/코테대비/HashMap_TreeSet/학급회장.java
@@ -1,5 +1,39 @@
 package 코테대비.HashMap_TreeSet;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class 학급회장 {
+     public static char solution(int n, String s){
+         char answer = ' ';
+         HashMap<Character, Integer> map = new HashMap<>();
+         for (char x : s.toCharArray()) {
+             map.put(x, map.getOrDefault(x, 0) + 1);
+         }
+//         System.out.println(map.containsKey('A'));
+//         System.out.println(map.size());
+//         System.out.println(map.remove('A'));
+//         System.out.println(map.size());
+
+         int max = Integer.MIN_VALUE;
+         for (char key : map.keySet()) {
+//             System.out.println(key+" "+ map.get(key));
+             if (map.get(key) > max) {
+                 max = map.get(key);
+                 answer = key;
+             }
+         }
+
+         return answer;
+     }
+
+     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+         String str = sc.next();
+
+         System.out.println(solution(n, str));
+     }
+
 
 }
