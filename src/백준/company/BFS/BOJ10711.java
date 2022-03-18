529ff9b730419a96be3501863f43f8d244395ed8
akdgns12 <akdgns12@naver.com>
2022-03-18 15:30:53 +0900

    모래성 BFS

---
+++ src/백준/company/BFS/BOJ10711.java
@@ -0,0 +1,117 @@
+package 백준.company.BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ10711 {
+    // 모래성 / 골드 3 / BFS
+    static int h, w;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1,-1,0,1,1,1,0,-1}; // 상, 우상, 우, 우하, 하, 좌하, 좌, 좌상
+    static int[] dy = {0,1,1,1,0,-1,-1,-1};
+    static Queue<Node> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        h = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+
+        map = new int[h][w];
+        visited = new boolean[h][w];
+        for (int i = 0; i < h; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < w; j++) {
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        // 주변의 모래인 부분 세서 같거나 크다면 큐에 넣기
+        int num = 0;
+        int cnt = 0;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if(map[i][j] == '.'){
+                    continue;
+                }
+
+                num = map[i][j] - '0';
+                if(num == 9) continue; // 9면 절대 무너지지 않음
+
+                cnt = checkCnt(i,j);
+
+                if(cnt >= num){
+                    q.offer(new Node(i,j));
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        System.out.println(bfs());
+    }
+
+    /*
+        초마다 무너져야 하는 모래성을 확인해 한번에 무너지도록 해야 한다.
+
+     */
+    static int bfs(){
+        int time = 0;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            for (int i=0; i<size; i++){
+                Node node = q.poll();
+
+                map[node.x][node.y] = '.';
+
+                for (int d = 0; d < 8; d++) {
+                    int nx = node.x + dx[d];
+                    int ny = node.y + dy[d];
+
+                    if(nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+
+                    if(map[nx][ny] == '.') continue;
+
+                    int num = map[nx][ny] - '0';
+                    if(!visited[nx][ny] && checkCnt(nx, ny) >= num){
+                        q.offer(new Node(nx,ny));
+                        visited[nx][ny] = true;
+                    }
+
+                }
+            }
+
+            time++;
+        }
+        return time;
+    }
+
+    static int checkCnt(int x, int y){
+        int cnt = 0;
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(map[nx][ny] == '.') {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    static class Node{
+        int x,y;
+
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
