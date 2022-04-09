903103c1f64d5782cc8814b4935a78068e9bede6
akdgns12 <akdgns12@naver.com>
2022-04-09 23:41:00 +0900

    최소신장트리 MST

--- src/복습/MST.java
+++ src/복습/MST.java
@@ -1,6 +1,4 @@
-package 백준.company.MST;
-
-import 코테대비.Greedy.친구인가_Union_Find;
+package 복습;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,8 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class BOJ1197 {
-    // 최소 스패닝 트리 / 골드 5 / MST
+public class MST {
     static int v, e;
     static int[] parent;
     static ArrayList<Edge> edges = new ArrayList<>();
@@ -23,7 +20,7 @@ public class BOJ1197 {
         v = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
 
-        // 부모테이블 상에서 자기자신으로 초기화
+        parent = new int[v+1];
         for (int i = 1; i <= v; i++) {
             parent[i] = i;
         }
@@ -32,51 +29,50 @@ public class BOJ1197 {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(cost, a, b));
+            int dist = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(dist, a, b));
         }
 
-        // 간선을 비용순으로 정렬
         Collections.sort(edges);
 
-        // 간선을 하나씩 확인하며
+
         for (int i = 0; i < edges.size(); i++) {
             int cost = edges.get(i).dist;
-            int a = edges.get(i).nodeA;
-            int b = edges.get(i).nodeB;
-            // 사이클이 발생하지 않는 경우에만 집합에 포함
+            int a = edges.get(i).a;
+            int b = edges.get(i).b;
+
             if(findParent(a) != findParent(b)){
-                unionParent(a,b);
+                union(a,b);
                 result += cost;
             }
         }
     }
 
-    // 특정 원소가 속한 집함을 찾기
     static int findParent(int x){
-        // 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
         if(x == parent[x]) return x;
         else return parent[x] = findParent(parent[x]);
     }
 
-    static void unionParent(int x, int y){
+    static void union(int x, int y){
         x = findParent(x);
         y = findParent(y);
-
 
         if(x < y) parent[y] = x;
         else parent[x] = y;
     }
 
-    static class Edge implements Comparable<Edge>{
+    static class Edge implements Comparable<Edge> {
         int dist;
-        int nodeA;
-        int nodeB;
-        public Edge(int dist, int nodeA, int nodeB){
+        int a;
+        int b;
+
+        public Edge(int dist, int a, int b) {
             this.dist = dist;
-            this.nodeA = nodeA;
-            this.nodeB = nodeB;
+            this.a = a;
+            this.b = b;
         }
+
         @Override
         public int compareTo(Edge o){
             return this.dist - o.dist;
