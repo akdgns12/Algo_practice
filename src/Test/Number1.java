28707a59f2c627c1e5eed7b8ac2a58a29ee51f20
akdgns12 <akdgns12@naver.com>
2022-04-11 17:19:23 +0900

    브론즈 실버 구현

--- src/Test/Number1.java
+++ src/Test/Number1.java
@@ -1,5 +1,9 @@
 package Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Number1 {
     public static void main(String[] args) {
         Number1 T = new Number1();
---
+++ src/백준/company/구현/BOJ10718.java
@@ -0,0 +1,10 @@
+package 백준.company.구현;
+
+import java.io.IOException;
+
+public class BOJ10718 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
