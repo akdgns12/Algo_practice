65d6ede79addf3fb5335e7efabba015a53367b87
akdgns12 <akdgns12@naver.com>
2022-04-12 15:26:32 +0900

    통계학 구현

--- src/백준/company/구현/BOJ2108.java
+++ src/백준/company/구현/BOJ2108.java
@@ -15,34 +15,19 @@ public class BOJ2108 {
         int n = Integer.parseInt(br.readLine());
 
         arr = new int[n];
-        int temp1 = 0;
-        int temp2 = 0;
-        int temp3 = 0;
-        int temp4 = 0;
+        double sum = 0;
 
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            arr[i] = num;
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
 
-        // 1. 산술평균
-        for (int i = 0; i < n; i++) {
-            temp1 += arr[i];
-        }
-        System.out.println(temp1 / n);
-
-        // 2. 중앙값
         Arrays.sort(arr);
-        int lt = 0;
-        int rt = n-1;
-        int mid = (lt + rt) / 2;
-        System.out.println(arr[mid]);
 
-        // 3. 최빈값
         int cnt = 0;
         int max = -1;
         int mod = arr[0];
-        boolean check = false;
+        boolean check = true; // 빈도수가 같을 때 두번째 값을 찾기위한 boolea 변수
         for (int i = 0; i < n - 1; i++) {
             if(arr[i] == arr[i+1]){
                 cnt++;
@@ -50,23 +35,22 @@ public class BOJ2108 {
                 cnt = 0;
             }
 
-            // 첫번째 작은 값
+            // 최빈값
             if(max < cnt){
                 max = cnt;
                 mod = arr[i];
                 check = true;
             }
-            // 두번째 작은 값
+            // 빈도수 같을 때 두번째 최빈값
             else if(max == cnt && check == true){
                 mod = arr[i];
                 check = false;
             }
         }
 
+        System.out.println(Math.round(sum / n));
+        System.out.println(arr[(n-1)/2]);
         System.out.println(mod);
-
-        // 4. 범위
         System.out.println(arr[n-1] - arr[0]);
-
     }
 }
