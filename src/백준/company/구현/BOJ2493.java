37f414d60d8e9d073baefd87edcf5d67784c3747
akdgns12 <akdgns12@naver.com>
2022-04-20 20:52:57 +0900

    탑 구현

---
+++ src/백준/company/구현/BOJ2493.java
@@ -0,0 +1,72 @@
+package 백준.company.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ2493 {
+    // 탑 / 골드 5 / 구현
+    /*
+        수평선에 일렬로 세워진 탑들의 신호를 각각 어느 탑에서 수신하는지 알아내라.
+        생각생각..
+        스택 사용하면 될 듯?
+        1. 스택이 비어있다면 0을 출력하고, 현재 탑을 스택에 push
+        2. 스택이 비어있지 않다면, 아래 2가지 case 검사
+         2-1. 스택에서 peek한 탑의 높이가 현재 탑의 높이보다 높다면, peek한 탑의 번호를 출력하고, 현재 탑을 스택에 push한다.
+         2-2. 스택에 peek한 탑의 높이가 현재 탑의 높이보다 낮다면, peek한 탑을 pop하고 2번으로 다시 돌아간다.
+     */
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        Stack<Top> stack = new Stack<>(); // 구조체를 이용하여 스택에 탑의 번호와, 높이 저장
+        ArrayList<Integer> result = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            int height = Integer.parseInt(st.nextToken());
+
+            if (stack.isEmpty()) { // 스택이 비어있다면, 0을 출력하고 탑을 push한다
+                result.add(0);
+                stack.push(new Top(i, height));
+            } else {
+                while(true){
+                    if(stack.isEmpty()){ // 스택이 비어있다면, 0을 출력하고 탑을 push한다.
+                        result.add(0);
+                        stack.push(new Top(i, height));
+                        break;
+                    }
+
+                    Top top = stack.peek();
+
+                    if (top.height > height) { // peek한 탑의 높이가 현재 탑의 높이보다 높다면,
+                        result.add(top.num); // peek한 탑의 번호를 출력하고,
+                        stack.push(new Top(i, height)); // 현재 탑을 스택에 push한다.
+                        break;
+                    } else { // peek한 탑의 높이가 현재 탑의 높이보다 낮다면,
+                        stack.pop(); // 스택에서 pop하고 다시 반복문을 돌린다.
+                    }
+                }
+            }
+        }
+
+        for (int i : result){
+            System.out.print(i + " ");
+        }
+    }
+
+    static class Top{
+        int num; // 탑의 번호
+        int height; // 탑의 높이
+
+        public Top(int num, int height) {
+            this.num = num;
+            this.height = height;
+        }
+    }
+}
--- src/복습/소수경로.java
+++ src/복습/소수경로.java
@@ -1,36 +1,27 @@
-package 백준.company.BFS;
+package 복습;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BOJ1963 {
-    // 소수 경로 / 골드4 / BFS
-    // 네자리 소수 a가 한자리씩 바꿀 때 b로 바꾸는 과정의 최소 횟수
-    // 0~9의 숫자를 가지는 네 개의 정점을 특정 조건(소수)을 만족하는 경우에만 탐색하여 원하는 목적지로 가는 최단경로! 를 구하는 문제
-    /*
-        생각
-        1. 주어진 범위 이하의 들 중 소수인 것들을 미리 판별해놓고
-        2. 입력받은 네자리수를 한자리씩 0~9범위 탐색 b와 같아질때까지(bfs탐색)
-     */
+public class 소수경로 {
     static int T;
     static boolean[] visited;
     static boolean[] isNotPrime = new boolean[10001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         T = Integer.parseInt(br.readLine());
-        // 4자리까지의 소수 미리 구한다.
-        // 이때, true는 소수가 아닌 수
         checkPrime();
 
         while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
@@ -47,7 +38,7 @@ public class BOJ1963 {
         while(!q.isEmpty()){
             Node now = q.poll();
 
-            if (now.num == target) {
+            if(now.num ==  target){
                 System.out.println(now.cnt);
                 return;
             }
@@ -56,17 +47,13 @@ public class BOJ1963 {
 
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 10; j++) {
-                    // 맨 앞자리인 천의자리가 0이면 1000을 초과하므로 pass
                     if(i == 0 && j == 0) continue;
 
-                    // 비밀변호 변경, pNum[i]번째 자리를 j로 바꿔서 그 자리수에 맞게 넣어줘야 함
                     int tmp = pNum[i];
                     pNum[i] = j;
                     int next = changePassword(pNum);
-                    // next가 하위 조건에 맞지 않을 경우 수 변환을 하지 말아야 하기 때문에 다시 원래대로 바꿔줌
                     pNum[i] = tmp;
 
-                    // 디음 숫자를 방문하지 않았고 없고, 소수이면 큐에 추가
                     if (!visited[next] && !isNotPrime[next]) {
                         q.offer(new Node(next, now.cnt + 1));
                         visited[next] = true;
@@ -77,27 +64,27 @@ public class BOJ1963 {
         System.out.println("IMPOSSIBLE");
     }
 
-    // 자리수에 맞게 다시 배치해주는 함수
     static int changePassword(int[] pNum) {
         int num = 0;
         for (int i = 0; i < 4; i++) {
-            num += pNum[i] * (Math.pow(10, 3 - i)); // 10의 (3-i)승 자리로 설정
+            num += pNum[i] * (Math.pow(10, 3 - i));
         }
         return num;
     }
 
     static void checkPrime(){
         for (int i = 2; i < 10000; i++) {
-            if(!isNotPrime[i]){ // i가 소수일경우
+            if(!isNotPrime[i]){ // i가 소수일때
                 for (int j = i * i; j < 10000; j += i) {
-                    isNotPrime[j] = true; // 소수인 i의 배수들은 모두 소수가 아님
+                    isNotPrime[j] = true; // i의 배수인 j들은 모두 소수가 아님
                 }
             }
         }
     }
 
     static class Node{
-        int num, cnt;
+        int num;
+        int cnt;
 
         public Node(int num, int cnt) {
             this.num = num;
