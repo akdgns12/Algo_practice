270aa89964208deedb13e53b7c7be91c3a6f1ec7
akdgns12 <akdgns12@naver.com>
2022-06-05 23:21:16 +0900

    탈출 BFS

--- src/백준/company/BFS/BOJ16397.java
+++ src/백준/company/BFS/BOJ16397.java
@@ -16,7 +16,7 @@ public class BOJ16397 {
         버튼 B를 눌러 N에 2를 곱한 순간 수가 99,999를 넘어간다면, 높은 자릿수의 수를 1 낮췄을때 99,999를 넘지 않는다고 해도 탈출에 실패하게 된다.
      */
     // 탈출에 필요한 최소 버튼 횟수
-    static int n,t,g;
+    static int N,T,G;
     static int[] click;
     static boolean[] visited;
 
@@ -24,78 +24,72 @@ public class BOJ16397 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        t = Integer.parseInt(st.nextToken()); // 버튼 클릭 최대 횟수
-        g = Integer.parseInt(st.nextToken()); // 탈출을 위해 똑같이 만들어야 하는 수
+        N = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken()); // 버튼 클릭 최대 횟수
+        G = Integer.parseInt(st.nextToken()); // 탈출을 위해 똑같이 만들어야 하는 수
 
         click = new int[100000];
         visited = new boolean[100000];
 
-        int ans = bfs(n);
-
-        System.out.println(ans != -1 ? ans : "ANG");
+        bfs(N);
     }
 
-    static int bfs(int n) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(n);
+    static void bfs(int n) {
+        Queue<Node> q = new LinkedList<>();
         visited[n] = true;
-        int result = -1;
+        q.offer(new Node(n, 0));
 
-        while (!q.isEmpty()) {
-            int cur = q.poll();
+        while(!q.isEmpty()){
+            Node node = q.poll();
 
-            if (cur == g) {
-                result = click[cur];
-            }
-            // 버튼 클릭횟수가 t 초과하면 pass
-            if(click[cur] + 1 > t) continue;
-
-            int a = cur + 1;
-            if(a < 100000 && !visited[a]){
-                visited[a] = true;
-                click[a] = click[cur] +  1;
-                q.offer(a);
+            if(node.t > T) break;
+            if (node.n == G) {
+                System.out.println(node.t);
+                return;
             }
 
-            int b = cur * 2;
+            for (int i = 0; i < 2; i++) {
+                if (i == 0) {
+                    int nx = node.n + 1;
+                    if (nx > 99999 || visited[nx]) continue;
+                    visited[nx] = true;
+                    q.offer(new Node(nx, node.t + 1));
+                } else {
+                    int nx = node.n * 2;
+                    if(nx > 99999 || visited[nx]) continue;
 
-            System.out.println(b);
+                    int idx = -1;
 
-            System.out.println();
+                    // 문제에서 정해진 자릿수 한계만큼 반복을 돌려서
+                    for (int j = 1; j < 7; j++) {
+                        // nx의 0이 아닌 가장 높은 자릿수는
+                        // nx를 나눴을 때 nx가 그대로 나머지가 되는 10의 j승 -1이니까
+                        if(nx % (int)Math.pow(10, j) == nx){
+                            idx = j; // 해당 j를 idx에 기억해놓고
+                            break;
+                        }
+                    }
 
-            if(b > 0 && b < 100000){
-                b = B(b);
-                System.out.println(b);
-                if(!visited[b]){
-                    visited[b] = true;
-                    click[b] = click[cur] + 1;
-                    q.offer(b);
+                    if (idx != -1) {
+                        nx -= (int) Math.pow(10, idx - 1); // nx에서 10의 idx-1승을 뺴준다 = nx에서 가장 높은 자릿수 -1하는 것과 동일
+                        if(visited[nx]) continue;
+                        visited[nx] = true;
+                        q.offer(new Node(nx, node.t + 1));
+                    }
                 }
             }
         }
 
-        return result;
+        System.out.println("ANG");
     }
 
-    // 0이 아닌 가장 높은 자리수를 1줄인다
-    static int B(int x){
-        int result = 0, idx = 10;
-        int[] arr = new int[5];
+    static class Node{
+        int n;
+        int t;
 
-        for (int i=0, k=10000; i<5; i++, k /= 10){
-            arr[i] = (x / k) % 10;
-
-            if(arr[i] != 0)
-                idx = Math.min(idx, i);
+        public Node(int n, int t) {
+            this.n = n;
+            this.t = t;
         }
-
-        arr[idx]--;
-
-        for (int i = 0, k = 10000; i < 5; i++, k /= 10) {
-            result += arr[i] * k;
-        }
-
-        return result;
     }
 }
