0db42c60658c635df142d4597bd41d6d9df75c72
akdgns12 <akdgns12@naver.com>
2022-06-15 14:49:23 +0900

    폴짝폴짝 BFS

--- src/복습/폴짝폴짝.java
+++ src/복습/폴짝폴짝.java
@@ -1,4 +1,4 @@
-package 백준.company.BFS;
+package 복습;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,15 +7,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BOJ1326 {
+public class 폴짝폴짝 {
     // 폴짝폴짝
     // 징검다리에 쓰여있는 수의 배수만큼 점프
     // a -> b 로 가려할 때 최소 몇번 점프하는지
-    /*
-        양의 방향, 음의 방향으로 움직이면서 방문체크하고
-        이런 과정은 목적지에 도착하기 전까지 진행하지만
-        결국 큐도 비게 된다는 것은 도달할 수 없다는 것이므로 -1을 반환
-     */
     static int n;
     static int[] arr;
     static boolean[] visited;
@@ -40,6 +35,11 @@ public class BOJ1326 {
         int b = Integer.parseInt(st.nextToken()) - 1;
 
         bfs(a, b);
+        if (minJump == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(minJump);
+        }
     }
 
     static void bfs(int start, int end) {
@@ -50,37 +50,46 @@ public class BOJ1326 {
         while (!q.isEmpty()){
             Node now = q.poll();
 
-            if (now.idx == end) { // 현재 위치가 목적지라면
-                System.out.println(now.jump);
+            if (now.idx == end) {
+                if(now.jump < minJump)
+                    minJump = now.jump;
                 return;
             }
 
-            // 아직 도착안했다면 계속 이동
-            // 양의 방향
-            for (int i = 1; now.idx + (arr[now.idx] * i) < n; i++) {
-                int next = now.idx + arr[now.idx] * i;
-                if(visited[next]) continue;
+            int multiple = 1;
 
-                visited[next] = true;
-                q.offer(new Node(next, now.jump + 1));
+            // right
+            while (true) {
+                int next = now.idx + arr[now.idx] * multiple;
+                if(next >= n) break;
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.offer(new Node(next, now.jump + 1));
+                }
+                multiple++;
             }
 
-            // 음의 방향
-            for (int i = 1; now.idx - (arr[now.idx] * i) >= 0; i++) {
-                int next = now.idx - arr[now.idx] * i;
-                if(visited[next]) continue;
+            multiple = 1;
 
-                visited[next] = true;
-                q.offer(new Node(next, now.jump + 1));
+            // left
+            while (true) {
+                int next = now.idx - arr[now.idx] * multiple;
+                if(next < 0) break;
+                if (!visited[next]) {
+                    q.offer(new Node(next, now.jump + 1));
+                    visited[next] = true;
+                }
+                multiple++;
             }
         }
 
-        System.out.println(-1);
+        return;
     }
 
+
     static class Node{
-        int idx; // 몇 번째 노드인지
-        int jump; // 점프 횟수
+        int idx;
+        int jump;
 
         public Node(int idx, int jump) {
             this.idx = idx;
