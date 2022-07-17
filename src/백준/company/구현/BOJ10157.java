545bfc209a9f6ddd61a9c9bdf2c8116898af661b
akdgns12 <akdgns12@naver.com>
2022-07-17 21:23:02 +0900

    SWEA D2 AL

---
+++ src/백준/company/구현/BOJ10157.java
@@ -0,0 +1,29 @@
+package 백준.company.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10157 {
+    // 자리배정 / 실버 4 / 구현
+    static int c, r;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        c = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        map = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < c; j++) {
+
+            }
+        }
+    }
+}
