942a17a585adb9828a4e22a3b9051bc6d24872ca
akdgns12 <akdgns12@naver.com>
2022-12-16 20:57:04 +0900

    BOJ12833 비트연산 XOR

---
+++ src/백준/company/구현/BOJ12833_XORXORXOR.java
@@ -0,0 +1,24 @@
+package 백준.company.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ12833_XORXORXOR {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken()) % 2;
+
+        // 세 수 A, B, C를 입력 받은 다음, ( ( ( ( A XOR B ) XOR B ) XOR B ) … ) XOR B 형태로 연산을 C회 했을 때의 결과값을 출력하는 프로그램을 작성하시오.
+        if(C == 0){
+            System.out.println(A);
+        }else{
+            System.out.println(A^B);
+        }
+    }
+}
